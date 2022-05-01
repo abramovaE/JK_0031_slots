5eeb8749ec8fefad8818df21dc441c345ab2d393
Ekaterina <kotofeya@yandex.ru>
2022-05-01 09:34:33 +0300

    1.0

--- app/src/main/java/com/template/MainActivity.kt
+++ app/src/main/java/com/template/MainActivity.kt
@@ -1,46 +1,84 @@
 package com.template
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import com.template.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var initCoins = 50
+    private var initCoins = 0
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences = getSharedPreferences("slots_sp", Context.MODE_PRIVATE)
+        initCoins = sharedPreferences.getInt("coins", 50)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setCoinsText()
 
-        binding.playBtn.setOnClickListener { v->
+        binding.playBtn.setOnClickListener {
             if(initCoins == 0) {
                 val someCoins = (0..100).random()
                 AlertDialog.Builder(this)
-                    .setMessage("You reward ${someCoins} coins")
-                    .setPositiveButton("Claim", { dialog, id ->
+                    .setMessage("You reward $someCoins coins")
+                    .setPositiveButton("Claim") { _, _ ->
                         initCoins += someCoins
                         setCoinsText()
-                    })
+                    }
                     .show()
             } else {
                 val intent = Intent(this, SlotsActivity::class.java)
                 intent.putExtra("coins", initCoins)
-                startActivity(intent)
+                getCoins.launch(intent)
+//                startActivity(intent)
             }
         }
-        binding.exitBtn.setOnClickListener { v->
-            this.finish()
+        binding.exitBtn.setOnClickListener {
+
+            Log.d("save coins", "" + initCoins)
+
+            val view = layoutInflater.inflate(R.layout.dialog_exit, null)
+            val dialog = AlertDialog.Builder(this)
+                .setView(view)
+                .show()
+
+            val timer = object: CountDownTimer(3000, 1500) {
+                override fun onTick(millisUntilFinished: Long) {
+                }
+                override fun onFinish() {
+                    dialog.dismiss()
+                    sharedPreferences.edit().putInt("coins", initCoins).apply()
+                    this@MainActivity.finish()
+                }
+            }
+            timer.start()
+
         }
     }
 
+    private val getCoins =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == Activity.RESULT_OK){
+                initCoins = it.data?.getIntExtra("coins", 0)!!
+                setCoinsText()
+                Log.d("coins", "coins: $initCoins")
+            }
+        }
 
     private fun setCoinsText(){
-        val coinsText = "Your coins: ${initCoins}"
-        binding.coinsTv.setText(coinsText)
+        val coinsText = "Your coins: $initCoins"
+        binding.coinsTv.text = coinsText
     }
 }
--- app/src/main/java/com/template/SlotsActivity.kt
+++ app/src/main/java/com/template/SlotsActivity.kt
@@ -1,29 +1,20 @@
 package com.template
 
+import android.app.Activity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.text.InputType
-import android.util.Log
-import android.view.animation.AnimationUtils
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.template.databinding.ActivitySlotsBinding
-
-import android.widget.TableLayout
-import android.widget.TableRow
-import android.widget.TextView
-
-
-
-
 
 class SlotsActivity: AppCompatActivity() {
     private lateinit var binding: ActivitySlotsBinding
 
     var coins = 0
     var bet = 0
-    val values = arrayOf("\uD83C\uDF53", "\uD83C\uDF4A", "\uD83C\uDF4B", "\uD83C\uDF47", "\uD83E\uDD5D")
+    private val values = arrayOf("\uD83C\uDF53", "\uD83C\uDF4A", "\uD83C\uDF4B", "\uD83C\uDF47", "\uD83E\uDD5D")
     var textViews = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,31 +29,33 @@ class SlotsActivity: AppCompatActivity() {
         initTextViewList()
 
 
-        binding.betBtn.setOnClickListener { v->
+        binding.betBtn.setOnClickListener {
             val editText = EditText(this)
             editText.inputType = InputType.TYPE_CLASS_NUMBER
             AlertDialog.Builder(this)
                 .setMessage("Input coins count")
                 .setView(editText)
-                .setPositiveButton( "OK", {
-                    dialog, id ->
+                .setPositiveButton( "OK") { _, _ ->
                     bet = editText.text.toString().toInt()
                     setBetBtnText()
-                })
+                }
                 .show()
         }
 
         binding.menuBtn.setOnClickListener {
-                v -> finish()
+            intent.putExtra("coins", coins)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
         }
 
 
 
-        var adapter = EmojiAdapter(textViews, this)
+        val adapter = EmojiAdapter(textViews, this)
         binding.rv.adapter = adapter
 
 
-        binding.spinBtn.setOnClickListener { v ->
+        binding.spinBtn.setOnClickListener {
+            binding.spinBtn.isClickable = false
 
             val timer = object: CountDownTimer(5000, 1500) {
                 override fun onTick(millisUntilFinished: Long) {
@@ -73,77 +66,28 @@ class SlotsActivity: AppCompatActivity() {
                 override fun onFinish() {
                     coins = coins - bet + (-5 until 5).random()
                     setCoinsText()
+                    binding.spinBtn.isClickable = true
+
                 }
             }
             timer.start()
-
-
-
-
-
-
-
         }
-
-
-
     }
 
-//    private fun startAnim(){
-//        var tableLayout = binding.tableLayout
-//        for (i in 0 until tableLayout.childCount) {
-//            val row = tableLayout.getChildAt(i) as TableRow
-//            for (j in 0 until row.childCount) {
-//                val tv = row.getChildAt(j) as TextView
-//                var anim = AnimationUtils.loadAnimation(this, R.anim.incr);
-//                tv.startAnimation(anim)
-//            }
-//        }
-//    }
-
-    private fun stopAnim(){}
-
     private fun setCoinsText(){
-        var coinsTvText = "Coins: ${coins}"
-        binding.coinsTv.setText(coinsTvText)
+        val coinsTvText = "Coins: $coins"
+        binding.coinsTv.text = coinsTvText
     }
 
     private fun setBetBtnText(){
-        binding.betBtn.setText("Bet: ${bet}")
+        val text = "Bet: $bet"
+        binding.betBtn.text = text
     }
 
     private fun initTextViewList() {
         for (i in 0 until 9){
-            var r = (0 until 5).random()
+            val r = (0 until 5).random()
             textViews.add(values[r])
-        }
-    }
-
-    private fun fillRv(){
-
-    }
-    
-    private fun fillTableLayout(){
-//        val tableLayout = binding.tableLayout
-//        tableLayout.removeAllViews()
-
-        var k = 0;
-        for (i in 0 until 3) {
-            val tableRow = TableRow(this)
-            tableRow.setLayoutParams(
-                TableLayout.LayoutParams(
-                    TableLayout.LayoutParams.MATCH_PARENT,
-                    TableLayout.LayoutParams.MATCH_PARENT
-                )
-            )
-            for (j in 0 until 3) {
-                val textView = TextView(this)
-                textView.text = textViews.get(k)
-                textView.textSize = 50.0f
-                k++
-                tableRow.addView(textView, j)
-            }
-//            tableLayout.addView(tableRow, i)
         }
     }
 }
